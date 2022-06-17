7c59e63d446f85cb5aaba7bc10ec9af2dc7de0b5
AnupamTekale <65555514+AnupamTekale@users.noreply.github.com>
2022-06-17 19:51:15 +0530

    Create voting.sol
    Demo code (Without front-end) for the decentralized voting application using BlockChain.

---
+++ voting.sol
@@ -0,0 +1,75 @@
+pragma solidity >= 0.7.0 < 0.9.0;
+
+contract Ballot{
+    
+
+    struct Voter{
+        uint vote;
+        bool voted;
+        uint weight;
+    }
+    struct Proposal{
+        bytes32 name;
+        uint voteCount;
+    }
+
+    Proposal [] public proposals;
+    mapping(address=> Voter) voters;
+
+    address public chairPerson;
+
+    constructor(bytes32[] memory proposalNames){
+
+        chairPerson = msg.sender;
+        voters[chairPerson].weight=1;
+        for(uint i=0;i<proposalNames.length;i++)
+        {
+            proposals.push(Proposal({
+                name : proposalNames[i],
+                voteCount : 0
+            }));
+        }
+
+    }
+
+    function giveRightToVote(address voter) public
+    {
+        require(msg.sender==chairPerson,
+                'Only the ChairPerson can give access to vote');
+        
+        require(!voters[voter].voted,
+                'The voter has already voted');
+
+        require(voters[voter].weight==0);
+
+        voters[voter].weight=1;
+    }
+
+
+    function vote(uint proposal) public{
+        Voter storage sender = voters[msg.sender];
+        require(sender.weight!=0,"He has no right to Vote");
+        require(!sender.voted,'Already voted');
+        sender.voted=true;
+        sender.vote = proposal;
+
+        proposals[proposal].voteCount += sender.weight;
+        
+    }
+    
+    function winningProposal() public view returns(uint winningProposal_){
+        uint winningVoteCount=0;
+        for(uint i=0;i<proposals.length;i++)
+        {
+            if(proposals[i].voteCount> winningVoteCount)
+            winningVoteCount = proposals[i].voteCount;
+            winningProposal_ = i;
+        }
+    }
+
+    function winningName() public view returns(bytes32 winningName_)
+    {
+        winningName_ = proposals[winningProposal()].name;
+    }
+    
+}
